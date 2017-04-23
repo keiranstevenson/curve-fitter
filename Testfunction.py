a20d4047e305673282fef0f8592e868277110d2d
Keiran Stevenson <s1002426@staffmail.ed.ac.uk>
2017-04-24 00:42:53 +0100

    Added normalise and align functions

--- Testfunction.py
+++ Testfunction.py
@@ -5,20 +5,43 @@ Created on Fri Apr 21 17:12:28 2017
 @author: s1002426
 """
 
-import platereaderimport as pi
 from glob import glob
 import pandas as pd
-header = None
-skiprows = 6
-filedirectory = 'C:\Data\Gitstuff\Curve-fitter\Datafiles'
+import numpy as np
+import os
+import platereaderimport as pltim
 
 
-files = glob(filedirectory + '/*.csv')
-#files = [file.split('/')[-1] for file in files]
-for i in range(0,(len(files)-1)):
+filename = 'Datafiles/truncatedtest.CSV'
+skiprows= 6
+labelcols= 3
+header= None
+
+infile = pd.read_csv(filename, header=header, skiprows=skiprows)
+normvalue= 0.05
+alignvalue= 0.1
+
+pltim.normalisetraces(infile)
+dataset = np.array(infile.iloc[2:11,labelcols:])
+alignpoint= normvalue+alignvalue
+startindexes = np.int_(dataset[:,0])
+
+for i in range(0,dataset.shape[0]):
+    for ii in range(0,dataset.shape[1]):
+        if (dataset[i,ii]> alignpoint) & (dataset[i,ii+1]> alignpoint) & (dataset[i,ii+2]> alignpoint):
+            x = ii
+            break
+    startindexes[i] = np.int_(x)
+    
+minindex = startindexes.min()
+maxindex = startindexes.max()
+maxrowlength = dataset.shape[1]-maxindex-1
+for i in range(0,dataset.shape[0]):
+    rowindex = startindexes[i]
+    newdata = dataset[i,rowindex-minindex:rowindex+maxrowlength-1]
     if i == 0:
-        stackfile = pd.read_csv(files[i],header= header,skiprows= skiprows)
+        stack= newdata
     else:
-        newfile = pd.read_csv(files[i],header= header,skiprows= skiprows+1)
-        stack = [stackfile,newfile]
-        stackfile = pd.concat(stack)
+        stack= np.vstack((stack,newdata))
+
+
--- platereaderimport.py
+++ platereaderimport.py
@@ -13,8 +13,8 @@ import os
 from glob import glob
 
 
-def platereaderimport(filename,header= None, manufacturer= 'BMG', skiprows= 0, labelcols = 1, 
-                      waterwells = 1, replicates = 0, growthmin = 0.05,
+def platereaderimport(filename,header= None, predefinedinput= 'BMG', skiprows= 0, labelcols = 1, replicols = 2, repignore = None,
+                      waterwells = 1, replicates = 0, normalise = 0.05, growthmin = 0.05, 
                       fitparams= {0: [-5,8],1: [-6,-1], 2: [-5,2]}, noruns= 5, nosamples= 20,
                       makeplots = 1):
     # filename or folder location, if latter then all files imported and processed
@@ -27,28 +27,45 @@ def platereaderimport(filename,header= None, manufacturer= 'BMG', skiprows= 0, l
     # noruns; number of fitting attempts made
     # nosamples; number of samples used to calculate error
     
-    if (manufacturer == 'BMG'):
+    if (predefinedinput == 'BMG'):
         skiprows= 6
         labelcols = 3
-    elif (manufacturer == 'Tecan'):
+        repignore = 'Sample X*'
+        
+    elif (predefinedinput == 'Tecan'):
         skiprows= 63
         labelcols = 1
+        if replicates == 1 & replicols > labelcols:
+            replicates = 0
         
-    if makeplots == 1: # makes directory for plots to be put in
-        if not os.path.isdir(filename.split('/')[-2] + '/plots/'):
-            os.makedirs(filename.split('/')[-2] + '/plots/') 
-        
+
     # Process files before inputting
+    filename = os.path.realpath(filename)
     if replicates == 1 & os.path.isdir(filename)==True:
         infile = multifilerepimport(filename, header, skiprows, labelcols, waterwells)
-    elif os.path.isdir(filename):
-        replicates == 1
+        filepath = filename + '/' + filename.split('/')[-1] + ' outputdata'
+        filename = filename.split('/')[-1]
+        
+#    elif os.path.isdir(filename):
+#       replicates == 1
     else:
         infile = pd.read_csv(filename, header=header, skiprows=skiprows)
+        filepath = os.path.split(filename)[0]
+        filename = os.path.split(filename)[1]
+        filepath = filepath + '/' + filename.split('.')[-2] + ' outputdata'
     
         if waterwells == 1:
             infile = removewaterwells(infile,labelcols,0)    
+
+    if not os.path.isdir(filepath):
+        os.makedirs(filepath)       
         
+        
+    if makeplots == 1: # makes directory for plots to be put in
+        if not os.path.isdir(filepath + '/plots/'):
+            os.makedirs(filepath + '/plots/') 
+    
+
     
     # Prepare output variables with names
     growthrates = infile.iloc[1:,0:7]
@@ -115,15 +132,15 @@ def platereaderimport(filename,header= None, manufacturer= 'BMG', skiprows= 0, l
                 plt.xlabel('Time [h]')
 
                 if replicates == 1:
-                    picname = str(infile.iloc[i,2])
+                    picname = str(infile.iloc[i,replicols])
                         
-                elif manufacturer == 'BMG':
+                elif predefinedinput == 'BMG':
                     picname = str(infile.iloc[i,0]) + str(int(infile.iloc[i,1]))
                     
                 else:
                     picname = str(infile.iloc[i,0])
                 
-                picname = filename.split('/')[0] + '/plots/' + picname + '.PNG'
+                picname = filepath + '/plots/' + picname + '.PNG'
                 plt.savefig(picname)
                 plt.show()
                         
@@ -157,7 +174,7 @@ def platereaderimport(filename,header= None, manufacturer= 'BMG', skiprows= 0, l
         growthcurvesder.ix[i,labelcols:datalength+labelcols-1] = fitdercurve
         growthcurvesdererr.ix[i,labelcols:datalength+labelcols-1] = fitdercurveerr
         
-    Outputname = filename.split('.')[-2] + '_GRoutput.xlsx'
+    Outputname = filepath + '/' + filename + ' Analysed.xlsx'
     writer = pd.ExcelWriter(Outputname, engine='xlsxwriter')
     growthrates.to_excel(writer, sheet_name='Stats')
     growthcurves.to_excel(writer, sheet_name='fit')
@@ -185,14 +202,15 @@ def removewaterwells(indata,labelcols,deletewells):
                 data.iloc[[i],3:] = np.zeros(data.shape[1]-3)
     return data
             
+            
 def multifilerepimport(filedirectory, header, skiprows, labelcols, waterwells):
     files = glob(filedirectory + '/*.csv')
     #files = [file.split('/')[-1] for file in files]
     for i in range(0,(len(files)-1)):
         if i == 0:
-            stackfile = pd.read_csv(files[i],header= header,skiprows= skiprows)
+            stackfile = pd.read_csv(files[i], header= header, skiprows= skiprows)
         else:
-            newfile = pd.read_csv(files[i],header= header,skiprows= skiprows+1)
+            newfile = pd.read_csv(files[i], header= header, skiprows= skiprows+1)
             stack = [stackfile,newfile]
             stackfile = pd.concat(stack)
     if waterwells == 1:
@@ -200,3 +218,48 @@ def multifilerepimport(filedirectory, header, skiprows, labelcols, waterwells):
    
     return stackfile
     
+    
+def normalisetraces(indata, normvalue= 0.05, labelcols= 3, timerow= 1):
+
+    data = indata.iloc[timerow:,labelcols:]
+    # Normalises line by line on points 5:15
+    for i in range(0,data.shape[0]):
+        datasnip = data.iloc[i,:]
+        datasnip = np.array(datasnip)
+        zeroingvalue= np.mean(datasnip[4:14])
+        zeroingvalue= normvalue - zeroingvalue
+        newnumline = datasnip + zeroingvalue
+        
+        if i == 0:
+            stack= newnumline
+        else:
+            stack= np.vstack((stack,newnumline))
+    indata.iloc[1:,labelcols:] = stack 
+    return    
+
+
+def alignreplicates(dataset, normvalue= 0.05, alignvalue= 0.1):
+    alignpoint= normvalue+alignvalue
+    startindexes = np.int_(dataset[:,0])
+    
+    # Finds where data > alignpoint for 3 consecutive points
+    for i in range(0,dataset.shape[0]):
+        for ii in range(0,dataset.shape[1]):
+            if (dataset[i,ii]> alignpoint) & (dataset[i,ii+1]> alignpoint) & (dataset[i,ii+2]> alignpoint):
+                x = ii
+                break
+        startindexes[i] = np.int_(x)
+        
+    # Aligns data into array by dropping cells in everything but lowest lag set
+    minindex = startindexes.min()
+    maxindex = startindexes.max()
+    maxrowlength = dataset.shape[1]-maxindex-1
+    for i in range(0,dataset.shape[0]):
+        rowindex = startindexes[i]
+        newdata = dataset[i,rowindex-minindex:rowindex+maxrowlength-1]
+        if i == 0:
+            stack= newdata
+        else:
+            stack= np.vstack((stack,newdata))
+    return stack
+
